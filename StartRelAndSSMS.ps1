3d1393a6b87179725d8cdc3acce645b3d78c55db
GitSalman <salman.s.iqbal@gmail.com>
2019-12-09 20:20:33 -0600

    Automated Tasks in Hopper
    StartRelAndSSMS.ps1 - automated starting Relativity front-end and SSMS
    StartRelServices.ps1 - automated starting Relativity services

---
+++ StartRelAndSSMS.ps1
@@ -0,0 +1,43 @@
+﻿#Script to start Chrome and sign into Relativity, then start SSMS
+
+$chromePath = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+$ssmsPath = "C:\Program Files (x86)\Microsoft SQL Server\130\Tools\Binn\ManagementStudio\Ssms.exe"
+
+
+#Starting Chrome
+Write-Host 'Starting Google Chrome...'
+
+Start-Process -FilePath $chromePath
+
+$chrome = Get-Process chrome -ErrorAction SilentlyContinue
+
+Sleep 5
+
+if ($chrome) {
+    Write-Host 'Google Chrome has started.'
+} else {
+    Write-Host 'Google Chrome failed to start.'
+}
+
+
+[System.Windows.Forms.SendKeys]::SendWait("relativity.admin@relativity.com")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Sleep 2
+[System.Windows.Forms.SendKeys]::SendWait("Password@1")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+
+#Starting SSMS
+Write-Host 'Starting SQL Server Management Studio...'
+Start-Process -FilePath $ssmsPath
+$ssms = Get-Process Ssms -ErrorAction SilentlyContinue
+
+Sleep 30
+
+if ($ssms) {
+    Write-Host 'SQL Server Management Studio has started.'
+} else {
+    Write-Host 'SQL Server Management Studio failed to start.'
+}
+
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
---
+++ StartRelServices.ps1
@@ -0,0 +1,38 @@
+﻿$SHM = Get-Service -Name 'kCura Service Host Manager'
+$WM = Get-Service -Name 'kCura EDDS Web Processing Manager'
+$AM = Get-Service -Name 'kCura EDDS Agent Manager'
+$IQM = Get-Service -Name 'Invariant Queue Manager'
+
+$services = @($WM,$SHM,$AM,$IQM)
+
+$msgBoxInput = [System.Windows.MessageBox]::Show("Would you like to start up Relativity Services on $env:computername ?",'Start Relativity Services','YesNoCancel','Error')
+
+switch ($msgBoxInput) {
+
+'Yes' {
+
+    foreach ($service in $services) {
+        while ($service.Status -ne 'Running') {
+            Start-Service $service
+            write-host $service.Status
+            write-host 'Service starting'
+            Start-Sleep -seconds 60
+            $service.Refresh()
+            if ($service.Status -eq 'Running')
+            {
+                Write-Host "$service service is now Running"
+            }
+        }
+    }
+
+}
+
+'No' {
+    function ex{exit}
+}
+
+'Cancel' {
+    function ex{exit}
+}
+
+}
